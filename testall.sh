7cda35e2247206e390117b1dd932db199778db9f
Shun <20508894+sinmentis@users.noreply.github.com>
2018-05-26 22:04:16 +1200

    Use this to start up all default router

--- testall.sh
+++ testall.sh
@@ -1,7 +1,7 @@
-python3 Test.py test1.txt&
-gnome-terminal -e "python3 Test.py test2.txt"
-gnome-terminal -e "python3 Test.py test3.txt"
-gnome-terminal -e "python3 Test.py test4.txt"
-gnome-terminal -e "python3 Test.py test5.txt"
-gnome-terminal -e "python3 Test.py test6.txt"
-gnome-terminal -e "python3 Test.py test7.txt"
+python3 Router.py test1.txt&
+gnome-terminal -e "python3 Router.py test2.txt"
+gnome-terminal -e "python3 Router.py test3.txt"
+gnome-terminal -e "python3 Router.py test4.txt"
+gnome-terminal -e "python3 Router.py test5.txt"
+gnome-terminal -e "python3 Router.py test6.txt"
+gnome-terminal -e "python3 Router.py test7.txt"
